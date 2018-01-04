0d17493ccd309547076b13926b5d0a05c74877fc
Dan Cahill <dcahill@nulllogic.ca>
2018-01-03 19:01:23 -0500

    started C# tray host project with NS and C# scripting

---
+++ src/hosts/ntray.net/CSScripter.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Windows.Forms;
+//using Microsoft.CSharp.;
+
+namespace NTray_NET
+{
+	public class CSScripter
+	{
+		public CSScripter()
+		{
+		}
+
+		public static bool testbool = false;
+
+		public void Test()
+		{
+			try
+			{
+				//string src = "using System.Windows.Forms;\r\nclass Test\r\n{\r\nstatic void Main(string[] args)\r\n{\r\nMessageBox.Show(\"Hello World!\", \"NScript\");\r\n}\r\n};";
+
+				AppDomain domain = AppDomain.CreateDomain("MyDomain");
+				Console.WriteLine("Host domain: " + AppDomain.CurrentDomain.FriendlyName);
+				Console.WriteLine("child domain: " + domain.FriendlyName);
+
+				CompilerParameters compilerparams = new CompilerParameters();
+				compilerparams.ReferencedAssemblies.Add(Application.ExecutablePath);
+				compilerparams.ReferencedAssemblies.Add("System.Windows.Forms.dll");
+				compilerparams.GenerateInMemory = true;
+				compilerparams.GenerateExecutable = true;
+
+				CodeDomProvider provider = new Microsoft.CSharp.CSharpCodeProvider();
+				CompilerResults results = provider.CompileAssemblyFromFile(compilerparams, System.IO.Path.Combine(Application.StartupPath, "NTray.NET.cs"));
+				//System.CodeDom.Compiler.CompilerResults results = provider.CompileAssemblyFromSource(compilerparams, src);
+
+				if (results.Errors.HasErrors)
+				{
+					string s = "";
+					for (int i = 0; i < 10 && i < results.Errors.Count; i++)
+					{
+						if (i > 0) s += "\r\n";
+						s += string.Format("Line {0}: {1}", results.Errors[i].Line, results.Errors[i].ErrorText);
+					}
+					throw new Exception(s);
+				}
+
+				results.CompiledAssembly.EntryPoint.Invoke(null, System.Reflection.BindingFlags.Static, null, new object[] { new string[] { "a", "b" } }, null);
+				//results.CompiledAssembly.EntryPoint.Invoke(null, System.Reflection.BindingFlags.Instance, null, new object[] { args }, null);
+
+				Assembly generatedAssembly = results.CompiledAssembly;
+				//generatedAssembly.CreateInstance("Test");
+				Type type = generatedAssembly.GetType("Test");
+				object obj = Activator.CreateInstance(type);
+
+				//var obj = Activator.CreateInstance(type);
+				//type.InvokeMember("Main2", BindingFlags.Default | BindingFlags.InvokeMethod, null, obj, null);
+				//type.InvokeMember("Main2", BindingFlags.Default | BindingFlags.InvokeMethod, null, obj, new string[] { });
+
+				//method.Invoke(obj, null);
+				type.GetMethod("Main2").Invoke(obj, new object[] { new string[] { "a", "b" } });
+				type.GetMethod("Main3").Invoke(obj, new object[] { });
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message, "assembly exception", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+			Console.WriteLine("NTray_NET.CSScripter.testbool: " + NTray_NET.CSScripter.testbool);
+		}
+	}
+}
---
+++ src/hosts/ntray.net/Program.cs
@@ -0,0 +1,70 @@
+﻿//https://alanbondo.wordpress.com/2008/06/22/creating-a-system-tray-app-with-c/
+//https://social.msdn.microsoft.com/Forums/vstudio/en-US/0913ae1a-7efc-4d7f-a7f7-58f112c69f66/c-application-system-tray-icon?forum=csharpgeneral
+
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace NTray_NET
+{
+	public class SysTrayApp : Form
+	{
+		[STAThread]
+		public static void Main()
+		{
+			Application.Run(new SysTrayApp());
+		}
+
+		private NotifyIcon trayIcon;
+		private ContextMenu trayMenu;
+
+		public SysTrayApp()
+		{
+			try
+			{
+				trayMenu = new ContextMenu();
+				trayMenu.MenuItems.Add("Test Script", TestScript);
+				trayMenu.MenuItems.Add("Exit", OnExit);
+				trayIcon = new NotifyIcon();
+				trayIcon.Text = "NTray.NET\nPowered by wishful thinking";
+				trayIcon.Icon = new Icon(System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream("NTray.NET.NTray.NET.ico"));
+				trayIcon.ContextMenu = trayMenu;
+				trayIcon.Visible = true;
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+				Application.Exit();
+				this.Close();
+			}
+		}
+
+		private void TestScript(object sender, EventArgs e)
+		{
+			//throw new NotImplementedException();
+			CSScripter c = new CSScripter();
+			c.Test();
+		}
+
+		protected override void OnLoad(EventArgs e)
+		{
+			Visible = false;
+			ShowInTaskbar = false;
+			base.OnLoad(e);
+		}
+
+		private void OnExit(object sender, EventArgs e)
+		{
+			Application.Exit();
+		}
+
+		protected override void Dispose(bool isDisposing)
+		{
+			if (isDisposing)
+			{
+				trayIcon.Dispose();
+			}
+			base.Dispose(isDisposing);
+		}
+	}
+}
