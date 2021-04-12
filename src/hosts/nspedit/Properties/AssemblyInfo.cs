c6e4dcf14c72b867046d483dcdd16fcedb90db49
Dan Cahill <nulllogic@gmail.com>
2021-04-11 20:17:17 -0400

    changed framework version to 4.6.2

--- src/hosts/nspedit/Properties/AssemblyInfo.cs
+++ src/hosts/nspedit/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NSPEdit")]
-[assembly: AssemblyCopyright("Copyright © Dan Cahill 2020")]
+[assembly: AssemblyCopyright("Copyright © Dan Cahill 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
