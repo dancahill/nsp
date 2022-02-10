9a2d67cca45ed472a1352276c4e027b521d8f444
Dan Cahill <nulllogic@gmail.com>
2022-02-10 17:57:00 -0500

    changes copyright year to 2022

--- src/hosts/nspedit/Properties/AssemblyInfo.cs
+++ src/hosts/nspedit/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NSPEdit")]
-[assembly: AssemblyCopyright("Copyright © Dan Cahill 2021")]
+[assembly: AssemblyCopyright("Copyright © Dan Cahill 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
